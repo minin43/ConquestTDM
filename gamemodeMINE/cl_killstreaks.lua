635f64134fbf226071ed4c761e5393a0ee142932
Drained-chan <stealth@mcfx2.co>
2019-03-28 20:23:26 -0500

    Removed deprecated gamemodeMINE folder

--- gamemodeMINE/cl_killstreaks.lua
+++
@@ -1,7 +0,0 @@
-net.Receive("KillstreakNotification", function()
-    local plyName = net.ReadString()
-    local soundName = net.ReadString()
-    local num = net.ReadInt(16)
-    
-    chat.AddText(color_white, plyName .. " is on a " .. tostring(num) .. " kill streak!")
-end)
